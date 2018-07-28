b567726eef6e639fc254d49f14a2a47626ed17d0
Dan Sanduleac <dsanduleac@palantir.com>
2018-07-28 17:50:10 +0100

    Make scripts nicer

--- integration_test/testdata/unstoppable.sh
+++ integration_test/testdata/unstoppable.sh
@@ -5,5 +5,8 @@ echo "Hello, I am unstoppable $$"
 # Close fd 3 to signal we're ready
 exec 3>-
 
-# Close >&1 and >&2 so sleep doesn't inherit them
-sleep 10000 >/dev/null 2>&1
+# Close &1 and &2 so sleep doesn't inherit them
+exec 1>-
+exec 2>-
+
+sleep 10000

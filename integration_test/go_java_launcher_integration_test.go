fa23d19359e0f8a4baf7d3487b4b3b4c8d6d4d73
Chris Brockington-Hill <cbrockington@palantir.com>
2018-08-14 10:53:45 +0100

    go-java-launcher integration test

--- integration_test/go_java_launcher_integration_test.go
+++ integration_test/go_java_launcher_integration_test.go
@@ -19,11 +19,17 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
 	"testing"
+	"time"
 
 	"github.com/palantir/godel/pkg/products/v2/products"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/palantir/go-java-launcher/launchlib"
 )
 
 func TestMainMethod(t *testing.T) {
@@ -67,13 +73,66 @@ func TestCreatesDirs(t *testing.T) {
 	require.NoError(t, os.RemoveAll("bar"))
 }
 
+func TestSubProcessesStoppedWhenMainDies(t *testing.T) {
+	cmd := mainWithArgs(t, "testdata/launcher-static-multiprocess.yml", "testdata/launcher-custom-multiprocess-long-sleep.yml")
+	require.NoError(t, cmd.Start())
+
+	// give launcher time to spawn sub-processes
+	time.Sleep(200 * time.Millisecond)
+	ppid := cmd.Process.Pid
+	children := childProcesses(t, ppid)
+	assert.Len(t, children, 2, "there should be one sub-process and one monitor")
+
+	assert.NoError(t, cmd.Wait())
+
+	time.Sleep(launchlib.CheckPeriod + 500*time.Millisecond)
+	for _, pid := range children {
+		// This always succeeds on unix systems as it merely creates a process object
+		process, err := os.FindProcess(pid)
+		if err != nil {
+			continue
+		}
+
+		// Sending a signal of 0 checks the process exists, without actually sending a signal,
+		// see https://linux.die.net/man/2/kill
+		err = process.Signal(syscall.Signal(0))
+		if err != nil {
+			continue
+		}
+		assert.Fail(t, "child process was not killed", pid)
+	}
+}
+
 func runMainWithArgs(t *testing.T, staticConfigFile, customConfigFile string) (string, error) {
+	output, err := mainWithArgs(t, staticConfigFile, customConfigFile).CombinedOutput()
+	return string(output), err
+}
+
+func mainWithArgs(t *testing.T, staticConfigFile, customConfigFile string) *exec.Cmd {
 	cli, err := products.Bin("go-java-launcher")
 	require.NoError(t, err)
 
-	cmd := exec.Command(cli, staticConfigFile, customConfigFile)
-	outputBytes, err := cmd.CombinedOutput()
-	return string(outputBytes), err
+	return exec.Command(cli, staticConfigFile, customConfigFile)
+}
+
+func childProcesses(t *testing.T, pid int) map[string]int {
+	command := exec.Command("/bin/ps", "-o", "pid,command", "--no-headers", "--ppid", strconv.Itoa(pid))
+	output, err := command.CombinedOutput()
+	require.NoError(t, err)
+
+	children := map[string]int{}
+	for _, child := range strings.Split(strings.TrimSpace(string(output)), "\n") {
+		parts := strings.SplitN(strings.TrimSpace(child), " ", 2)
+		cpid, err := strconv.Atoi(parts[0])
+		cmdline := strings.TrimSpace(parts[1])
+		require.NoError(t, err)
+		// sleep forks into a separate process, we don't want to include it
+		if !strings.HasPrefix(cmdline, "sleep") {
+			children[strings.TrimSpace(parts[1])] = cpid
+		}
+	}
+
+	return children
 }
 
 func TestMain(m *testing.M) {

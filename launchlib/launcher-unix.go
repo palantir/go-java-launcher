9ed1c361aaa17be7ea574ff856dd7f294dcb8b69
Guilherme Silva <26658340+GuilhermeJSilva@users.noreply.github.com>
2026-07-23 13:38:41 +0100

    Remove unused windows flags

--- launchlib/launcher-unix.go
+++ launchlib/launcher-unix.go
@@ -3,10 +3,4 @@
 
 package launchlib
 
-import (
-	"os/exec"
-)
-
 const javaExecutablePath = "bin/java.exe"
-
-func setSysProcAttr(cmd *exec.Cmd) {}
--- launchlib/launcher-windows.go
+++ launchlib/launcher-windows.go
@@ -3,16 +3,4 @@
 
 package launchlib
 
-import (
-	"os/exec"
-
-	"golang.org/x/sys/windows"
-)
-
 const javaExecutablePath = "bin/java.exe"
-
-func setSysProcAttr(cmd *exec.Cmd) {
-	cmd.SysProcAttr = &windows.SysProcAttr{
-		CreationFlags: windows.CREATE_NEW_PROCESS_GROUP | windows.DETACHED_PROCESS,
-	}
-}
